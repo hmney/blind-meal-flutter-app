1bd5d586d3c27f89e3e72b88d80a8d8da597e610
hmney <hmney@student.1337.ma>
2020-08-12 23:10:36 +0100

    setup firebase

--- build/app/generated/source/buildConfig/debug/com/example/app/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/example/app/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.app;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.app";
+  public static final String APPLICATION_ID = "com.blind.meal";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
