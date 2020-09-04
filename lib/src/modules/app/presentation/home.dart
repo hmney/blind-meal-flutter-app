121741193857cd5c5c1b910391806f0908f9be38
hmney <hmney@student.1337.ma>
2020-09-04 05:41:54 +0100

    open drawer by click on leading icon

--- lib/src/modules/app/presentation/home.dart
+++ lib/src/modules/app/presentation/home.dart
@@ -1,4 +1,5 @@
 import 'package:app/src/core/styles.dart';
+import 'package:app/src/modules/auth/module.dart';
 import 'package:app/src/modules/auth/presentation/controller.dart';
 import 'package:app/src/modules/order/module.dart';
 import 'package:flutter/material.dart';
@@ -11,37 +12,49 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final _controller = Modular.get<AuthController>();
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       backgroundColor: AppTheme.backgroundColor,
       appBar: AppBar(
         backgroundColor: AppTheme.backgroundColor,
         elevation: 0,
-        leading: Icon(
-          Icons.menu,
-          color: Colors.black,
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+          onPressed: () => scaffoldKey.currentState.openDrawer(),
         ),
       ),
       drawer: Drawer(
-        child: ListView(
-          children: [
-            ListTile(
-              leading: Icon(
-                Icons.exit_to_app,
+        child: Center(
+          child: ListView(
+            children: [
+              ListTile(
+                leading: Icon(
+                  Icons.exit_to_app,
+                ),
+                title: Text('Log out'),
+                onTap: () async {
+                  Modular.to.popUntil((r) => r.isFirst);
+                  await _controller.addEvent(AuthEvent.logout());
+                },
               ),
-              title: Text('Log out'),
-              onTap: () async {
-                Modular.to.popUntil((r) => r.isFirst);
-                await _controller.addEvent(AuthEvent.logout());
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       body: Center(
         child: GestureDetector(
-          onTap: () => OrderModule.toOrderScreen(),
+          onTap: () {
+            _controller.currentUser.isSurveyFilled
+                ? OrderModule.toOrderScreen()
+                : AuthModule.toSurveyScreen();
+          },
           child: Container(
             width: 200,
             height: 200,
