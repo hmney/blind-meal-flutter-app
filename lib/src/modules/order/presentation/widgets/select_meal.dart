211e619b32cd3f2e4735fcb20aec6132747906f2
Yassine SABRI <ysabri@leyton.com>
2020-09-08 22:36:11 +0100

    WIP: add popup meal info

--- lib/src/modules/order/presentation/widgets/select_meal.dart
+++ lib/src/modules/order/presentation/widgets/select_meal.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:app/src/core/styles.dart';
 import 'package:app/src/modules/order/presentation/controller.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +14,7 @@ class SelectMeal extends StatefulWidget {
 class _SelectMealState extends State<SelectMeal> {
   List<String> recommendationFood = ["Burger", "Pizza", "Keto Salad", "Fish"];
   final controller = Modular.get<OrderController>();
+  OverlayEntry _overlayPopup;
 
   @override
   Widget build(BuildContext context) {
@@ -24,8 +27,15 @@ class _SelectMealState extends State<SelectMeal> {
         children: recommendationFood
             .map(
               (element) => Observer(
-                builder: (_) => GestureDetector(
+                builder: (context) => GestureDetector(
                   onTap: () => controller.setMealSelected(element),
+                  onLongPress: () {
+                    _overlayPopup = _createMealInfoPopUp(element);
+                    Overlay.of(context).insert(_overlayPopup);
+                  },
+                  onLongPressEnd: (_) {
+                    _overlayPopup?.remove();
+                  },
                   child: Stack(
                     children: <Widget>[
                       Container(
@@ -76,6 +86,122 @@ class _SelectMealState extends State<SelectMeal> {
               ),
             )
             .toList(),
+      ),
+    );
+  }
+
+  OverlayEntry _createMealInfoPopUp(String element) {
+    return OverlayEntry(
+      builder: (context) => AnimatedDialog(
+        child: _createPopupContent(element),
+      ),
+    );
+  }
+
+  // Main popup content
+  Widget _createPopupContent(String element) => Container(
+    padding: EdgeInsets.symmetric(horizontal: 20.0),
+    child: ClipRRect(
+      borderRadius: BorderRadius.circular(16.0),
+      child: Container(
+        height: MediaQuery.of(context).size.height / 3,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: AppTheme.primaryColor,
+                  width: 4,
+                  style: controller.mealSelected == element
+                      ? BorderStyle.solid
+                      : BorderStyle.none,
+                ),
+                image: DecorationImage(
+                  image: AssetImage(
+                      "assets/images/recommendedFood/$element.jpg"),
+                  fit: BoxFit.scaleDown,
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: <Color>[
+                    Colors.black.withAlpha(0),
+                    Colors.black12,
+                    Colors.black45
+                  ],
+                ),
+              ),
+              padding: const EdgeInsets.only(bottom: 4.0),
+              child: Container(
+                child: Text(
+                  element,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Georgia',
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+// This a widget to implement the image scale animation,
+// and blurred background effect
+class AnimatedDialog extends StatefulWidget {
+  const AnimatedDialog({Key key, this.child}) : super(key: key);
+
+  final Widget child;
+
+  @override
+  State<StatefulWidget> createState() => AnimatedDialogState();
+}
+
+class AnimatedDialogState extends State<AnimatedDialog> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> opacityAnimation;
+  Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 400));
+    scaleAnimation =
+        CurvedAnimation(parent: controller, curve: Curves.easeOutExpo);
+    opacityAnimation = Tween<double>(begin: 0.0, end: 0.6).animate(
+        CurvedAnimation(parent: controller, curve: Curves.easeOutExpo));
+
+    controller.addListener(() => setState(() {}));
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.black.withOpacity(opacityAnimation.value),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+        child: Center(
+          child: FadeTransition(
+            opacity: scaleAnimation,
+            child: ScaleTransition(
+              scale: scaleAnimation,
+              child: widget.child,
+            ),
+          ),
+        ),
       ),
     );
   }
