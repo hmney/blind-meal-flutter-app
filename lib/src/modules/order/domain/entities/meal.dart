c49c6254977f161efba83054e2bde3e37101e4b3
hmney <hmney@student.1337.ma>
2020-09-16 23:12:30 +0100

    meal class

---
+++ lib/src/modules/order/domain/entities/meal.dart
@@ -0,0 +1,36 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:equatable/equatable.dart';
+
+part 'meal.g.dart';
+
+@JsonSerializable()
+class Meal extends Equatable {
+  @JsonKey(name: 'uid')
+  final String id;
+  @JsonKey(name: 'title')
+  final String name;
+  @JsonKey(name: 'components')
+  final List<String> ingredients;
+  final String description;
+  final String image;
+  @JsonKey(name: 'original_price')
+  final String price;
+
+  Meal({
+    this.id,
+    this.name,
+    this.ingredients,
+    this.description,
+    this.image,
+    this.price,
+  });
+
+  @override
+  List<Object> get props => [id, name, ingredients, description, image, price];
+  factory Meal.fromJson(Map<String, dynamic> json) => _$MealFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MealToJson(this);
+
+  @override
+  bool get stringify => true;
+}
---
+++ lib/src/modules/order/domain/entities/meal.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'meal.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Meal _$MealFromJson(Map<String, dynamic> json) {
+  return Meal(
+    id: json['uid'] as String,
+    name: json['title'] as String,
+    ingredients:
+        (json['components'] as List)?.map((e) => e as String)?.toList(),
+    description: json['description'] as String,
+    image: json['image'] as String,
+    price: json['original_price'].toString(),
+  );
+}
+
+Map<String, dynamic> _$MealToJson(Meal instance) => <String, dynamic>{
+      'uid': instance.id,
+      'title': instance.name,
+      'components': instance.ingredients,
+      'description': instance.description,
+      'image': instance.image,
+      'original_price': instance.price,
+    };
