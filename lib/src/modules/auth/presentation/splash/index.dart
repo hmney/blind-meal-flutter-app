a315501e13b38aac924d6c7eb1f344127f8af140
hmney <hmney@student.1337.ma>
2020-09-16 23:11:33 +0100

    remove the image from splash screen

--- lib/src/modules/auth/presentation/splash/index.dart
+++ lib/src/modules/auth/presentation/splash/index.dart
@@ -11,27 +11,13 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Image(
-              image: AssetImage('assets/images/splash/splash.jpg'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          Positioned.fill(
-            child: Container(
-              color: AppTheme.primaryColor.withOpacity(0.5),
-            ),
-          ),
-          Center(
-            child: SvgPicture.asset(
-              'assets/logo/Logo-White.svg',
-              width: 260,
-              height: 260,
-            ),
-          ),
-        ],
+      backgroundColor: AppTheme.primaryColor,
+      body: Center(
+        child: SvgPicture.asset(
+          'assets/logo/Logo-White.svg',
+          width: 260,
+          height: 260,
+        ),
       ),
     );
   }
