627627abc0b25dfde8e0e038e9fac99958aac782
hmney <hmney@student.1337.ma>
2020-09-16 23:12:14 +0100

    add the connection between the app and the recommendation algorithm

--- lib/src/modules/order/data/repository/order_repository_implementation.dart
+++ lib/src/modules/order/data/repository/order_repository_implementation.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
+import 'package:app/src/modules/order/domain/entities/meal.dart';
 import 'package:app/src/modules/order/domain/entities/order.dart';
 import 'package:app/src/modules/order/domain/repository/order_repository.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dio/dio.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -8,10 +12,29 @@ class OrderRepositoryImplementation extends OrderRepository {
   final firestore = Firestore.instance;
   final analytics = Modular.get<FirebaseAnalytics>();
   CollectionReference get _orderCollection => firestore.collection('orders');
-
+  final url = 'http://51.210.249.13/query';
+  final dio = Dio();
   @override
   Future<void> createNewOrder(Order order) async {
     order.orderId = _orderCollection.document().documentID;
     await _orderCollection.document(order.orderId).setData(order.toJson());
+  }
+
+  @override
+  Future<List<Meal>> getRecommendedMeals(Order order) async {
+    try {
+      final response = await dio.post(
+        url,
+        data: order.toJson(),
+      );
+      if (response.statusCode == 200) {
+        return [for (var element in response.data) Meal.fromJson(element)];
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print(e);
+      return null;
+    }
   }
 }
